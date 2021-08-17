6941e188e01299071ef8c3b83a265f95ed1c34ec
Pedro Barbosa <pedro_dith@hotmail.com>
2021-08-17 20:26:19 -0300

    add sample navigation

--- fundamentos/src/pages/index.jsx
+++ fundamentos/src/pages/index.jsx
@@ -2,10 +2,16 @@ import Navegador from '../components/Navegador'
 
 export default function Inicio() {
    return (
-      <div>
-         <div>
-            <Navegador />
-         </div>
+      <div style={{
+         display: 'flex',
+         justifyContent: 'center',
+         alignItems: 'center',
+         flexWrap: 'wrap',
+         height: '100vh'
+      }}>
+         <Navegador texto="Estiloso" destino="/estiloso"/>
+         <Navegador texto="Exemplo" destino="/exemplo" cor="crimson"/>
+         <Navegador texto="Navegação #01" destino="/navegacao" cor="green"/>
       </div>
    )
 }
